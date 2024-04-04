ea4328cd10440acfb88faeded2adc3bf7ff47f98
Damien Knox <Deus@ASUS>
2024-04-05 11:37:58 +1300

    refactoring

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from tempfile import TemporaryDirectory
 
 import b2sdk.v2 as b2
 import requests
-from logdna import LogDNAHandler
 from prompt_toolkit import prompt
 from prompt_toolkit.validation import Validator, ValidationError
 from prompt_toolkit.shortcuts import radiolist_dialog
@@ -26,21 +25,29 @@ me = SingleInstance('pgbak')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-options = {
-    'app': 'pgbak',
-    'hostname': os.getenv('LOG_HOSTNAME')
-}
 
 MEZMO_INGESTION_KEY = os.environ['MEZMO_INGESTION_KEY']
-log_handler = LogDNAHandler(MEZMO_INGESTION_KEY, options)
-logger.addHandler(log_handler)
+if MEZMO_INGESTION_KEY:
+    from logdna import LogDNAHandler
+    hostname = os.getenv('LOG_HOSTNAME')
+    if not hostname:
+        import socket
+        hostname = socket.gethostname()
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(logging.Formatter("[pgbak] %(levelname)s: %(message)s"))
-logger.addHandler(handler)
+    options = {
+        'app': 'pgbak',
+        'hostname': hostname
+    }
 
-logging.getLogger("urllib3").setLevel(logging.ERROR)
-logging.getLogger("requests").setLevel(logging.ERROR)
+    log_handler = LogDNAHandler(MEZMO_INGESTION_KEY, options)
+    logger.addHandler(log_handler)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter("[pgbak] %(levelname)s: %(message)s"))
+    logger.addHandler(handler)
+
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    logging.getLogger("requests").setLevel(logging.ERROR)
 
 def parse_postgres_connection_string(connection_string):
     result = {}
@@ -68,9 +75,6 @@ def upload_to_b2(b2_key_id: str, b2_app_key: str, b2_bucket: str, backup_filenam
 
 
 def create_backup(pg_conn_string: str, backup_filename: str, archive_password):
-    # cmd = f'/usr/bin/pg_dump -d {pg_conn_string} | 7z a -si -p"{archive_password}" -mhe=on -mx=9 "{backup_filename}"'
-    # popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
-    # popen.wait()
     pg_dump_command = f'pg_dump -d {pg_conn_string} -F c -b -v'
     seven_zip_command = f'7z a -si -p"{archive_password}" -mhe=on -mx=9 {backup_filename}'
 
@@ -78,7 +82,7 @@ def create_backup(pg_conn_string: str, backup_filename: str, archive_password):
     seven_zip_process = subprocess.Popen(seven_zip_command, shell=True, stdin=pg_dump_process.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     pg_dump_process.stdout.close()
-    seven_zip_stdout, seven_zip_stderr = seven_zip_process.communicate()
+    _, seven_zip_stderr = seven_zip_process.communicate()
 
     dumb_err = pg_dump_process.stderr.read().decode("utf-8")
 
@@ -107,18 +111,20 @@ def run_backup(conn, force=False):
                 if hours_diff < row['frequency_hrs']:
                     continue
             try:
-                connection_string = row['connection_string']#f'postgres://{row["user"]}:{row["password"]}@{row["host"]}:{row["port"]}/{row["database"]}'
-                backup_filename = f'{row["archive_name"]}_{datetime.utcnow().strftime("%Y%m%dT%H%M%S")}.7z'
+                connection_string = row['connection_string']
+                #backup_filename = f'{row["archive_name"]}_{datetime.utcnow().strftime("%Y%m%dT%H%M%S")}.7z'
                 conn_details=parse_postgres_connection_string(connection_string)
-                logger.info(f'Create backup {conn_details["host"]} / {conn_details["database"]} to {backup_filename}')
+
+                logger.info(f'Creating backup {conn_details["host"]}/{conn_details["database"]} to {row["archive_name"]}')
                 archive_password = row['archive_password'] if row['archive_password'] else os.environ.get('ARCHIVE_PASSWORD')
-                create_backup(connection_string, backup_filename, archive_password)
-                filesize = os.path.getsize(backup_filename)
-                logger.info(f'Upload {backup_filename} to B2')
+                create_backup(connection_string, row["archive_name"], archive_password)
+                filesize = os.path.getsize(row["archive_name"])
+
+                logger.info(f'Uploading {row["archive_name"]} to B2')
                 b2_key_id = row['B2_KEY_ID'] if row['B2_KEY_ID'] else os.environ.get('B2_KEY_ID')
                 b2_app_key = row['B2_APP_KEY'] if row['B2_APP_KEY'] else os.environ.get('B2_APP_KEY')
                 b2_bucket = row['B2_BUCKET'] if row['B2_BUCKET'] else os.environ.get('B2_BUCKET')
-                uploaded_file = upload_to_b2(b2_key_id, b2_app_key, b2_bucket, backup_filename)
+                uploaded_file = upload_to_b2(b2_key_id, b2_app_key, b2_bucket, row["archive_name"])
                 logger.info(f'Success {uploaded_file=}')
 
                 conn.execute("""
@@ -127,6 +133,13 @@ def run_backup(conn, force=False):
                 conn.execute("UPDATE servers SET last_backup=?, last_backup_result='Success' WHERE id=?", (datetime.utcnow().strftime("%Y%m%dT%H%M%S"), row['id']))
                 if row['dms_id']:
                     requests.post(f"https://nosnch.in/{row['dms_id']}", data={"m": uploaded_file})
+
+                c = conn.execute('SELECT file_size FROM backup_log bl WHERE server_id=? ORDER BY ts DESC LIMIT 1',(row['id'],))
+                prev = c.fetchone()
+                c.close()
+                diff = abs(prev['file_size'] - filesize) / ((prev['file_size'] + filesize) / 2) * 100
+                if diff>10:
+                    logger.error(f'The file size of {conn_details["host"]}/{conn_details["database"]} differs from the previous one by {diff}%! Was: {prev["file_size"]}, now: {filesize}')
             except:
                 exc = traceback.format_exc()
                 conn.execute("""
@@ -156,13 +169,7 @@ class NotEmptyValidator(Validator):
 
 def command_add(conn):
     connection_string = prompt('connection_string: ', validator=NotEmptyValidator())
-    # host = prompt('host: ', validator=NotEmptyValidator())
-    # port = int(prompt('port: ', default='5432', validator=NumberValidator()))
-    # database = prompt('database: ', validator=NotEmptyValidator())
-    # user = prompt('user: ', validator=NotEmptyValidator())
-    # password = prompt('password: ', validator=NotEmptyValidator())
     frequency_hrs = int(prompt('frequency_hrs: ', validator=NumberValidator()))
-    keep_last_files = int(prompt('keep_last_files: ', validator=NumberValidator()))
     dms_id = prompt('dms_id: ')
     dms_id = None if dms_id == '' else dms_id
     B2_KEY_ID = prompt('B2_KEY_ID: ')
@@ -175,12 +182,10 @@ def command_add(conn):
     archive_password = prompt('archive_password: ')
     archive_password = None if archive_password == '' else archive_password
     conn.execute("""
-    --INSERT INTO servers (host, port, "database", "user", password, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, keep_last_files, archive_name, archive_password) 
-    INSERT INTO servers (connection_string, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, keep_last_files, archive_name, archive_password) 
-                  VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+    INSERT INTO servers (connection_string, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, archive_name, archive_password) 
+                  VALUES(?, ?, ?, ?, ?, ?, ?, ?)
     """,
-                 (connection_string, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, keep_last_files, archive_name, archive_password))
-                 #(host, port, database, user, password, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, keep_last_files, archive_name, archive_password))
+                 (connection_string, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, archive_name, archive_password))
 
 def command_edit(conn):
     c = conn.execute('select id, connection_string from servers')
@@ -204,13 +209,7 @@ def command_edit(conn):
 
 
     connection_string = prompt('connection_string: ', default=row['connection_string'],validator=NotEmptyValidator())
-    #host = prompt('host: ', default=row['host'],validator=NotEmptyValidator())
-    #port = int(prompt('port: ', default=str(row['port']), validator=NumberValidator()))
-    #database = prompt('database: ', default=row['database'], validator=NotEmptyValidator())
-    #user = prompt('user: ', default=row['user'], validator=NotEmptyValidator())
-    #password = prompt('password: ', default=row['password'], validator=NotEmptyValidator())
     frequency_hrs = int(prompt('frequency_hrs: ', default=str(row['frequency_hrs']), validator=NumberValidator()))
-    keep_last_files = int(prompt('keep_last_files: ', default=str(row['keep_last_files']), validator=NumberValidator()))
     dms_id = prompt('dms_id: ', default=row['dms_id'] if row['dms_id'] else '')
     dms_id = None if dms_id == '' else dms_id
     B2_KEY_ID = prompt('B2_KEY_ID: ', default=row['B2_KEY_ID'] if row['B2_KEY_ID'] else '')
@@ -223,11 +222,9 @@ def command_edit(conn):
     archive_password = prompt('archive_password: ', default=row['archive_password'] if row['archive_password'] else '')
     archive_password = None if archive_password == '' else archive_password
     conn.execute("""
-    --UPDATE servers SET host=?, port=?, "database"=?, "user"=?, password=?, frequency_hrs=?, dms_id=?, B2_KEY_ID=?, B2_APP_KEY=?, B2_BUCKET=?, keep_last_files=?, archive_name=?, archive_password=? 
-    UPDATE servers SET connection_string=?, frequency_hrs=?, dms_id=?, B2_KEY_ID=?, B2_APP_KEY=?, B2_BUCKET=?, keep_last_files=?, archive_name=?, archive_password=? 
+    UPDATE servers SET connection_string=?, frequency_hrs=?, dms_id=?, B2_KEY_ID=?, B2_APP_KEY=?, B2_BUCKET=?, archive_name=?, archive_password=? 
                   where id = ?
-    --(host, port, database, user, password, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, keep_last_files, archive_name, archive_password, row['id']))
-    """, (connection_string, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, keep_last_files, archive_name, archive_password, row['id']))
+    """, (connection_string, frequency_hrs, dms_id, B2_KEY_ID, B2_APP_KEY, B2_BUCKET, archive_name, archive_password, row['id']))
 
 def command_list(conn):
     c = conn.execute('SELECT * FROM servers')
@@ -237,27 +234,20 @@ def command_list(conn):
         print('Nothing here')
         return
     headers = list(rows[0].keys())
-    # rows = [list(d.values()) for d in rows]
-    table = tabulate(rows, headers, tablefmt="grid")
+    table = tabulate(rows, headers, tablefmt="grid", maxcolwidths=[20])
     print(table)
 
 
-def create_db_connect() -> sqlite3.Connection:
+def create_db_connection() -> sqlite3.Connection:
     if not os.path.isfile('/usr/local/etc/pgback/backup.sqlite'):
         os.makedirs('/usr/local/etc/pgback/', exist_ok=True)
         conn = sqlite3.connect('/usr/local/etc/pgback/backup.sqlite', isolation_level=None)
         create_tables_script = """
             CREATE TABLE servers (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                --host TEXT NOT NULL,
-                --port INTEGER DEFAULT (5432) NOT NULL,
-                --"database" TEXT NOT NULL,
-                --"user" TEXT NOT NULL,
-                --password TEXT NOT NULL,
                 connection_string TEXT,
                 frequency_hrs INTEGER DEFAULT (1) NOT NULL,                
                 B2_KEY_ID TEXT, B2_APP_KEY TEXT, B2_BUCKET TEXT,
-                keep_last_files INTEGER, 
                 archive_name TEXT, archive_password TEXT,
                 dms_id TEXT,
                 last_backup TEXT,
@@ -285,7 +275,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    conn: sqlite3.Connection = create_db_connect()
+    conn: sqlite3.Connection = create_db_connection()
 
     match args.command:
         case 'add':
