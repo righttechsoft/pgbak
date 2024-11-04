323c91375b8b6ce77aa9afd4624dbc5c301143b9
Damien Knox <Deus@ASUS>
2024-11-04 15:08:27 +1300

    logging added

--- main.py
+++ main.py
@@ -139,6 +139,9 @@ def create_backup(
     pg_dump_stderr = pg_dump_process.stderr.read()
     _, seven_zip_stderr = seven_zip_process.communicate()
 
+    logger.info(pg_dump_stderr.decode("utf-8"))
+    logger.info(seven_zip_stderr.decode("utf-8"))
+
 #    if pg_dump_process.returncode != 0:
 #        err = pg_dump_stderr.decode("utf-8")
 #        raise Exception(f'Error occurred during database dump:\n{err}')
